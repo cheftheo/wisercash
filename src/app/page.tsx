56bf991d38846c93c79ca74742c3ee3beae4c456
cheftheo <marascu.theodor@gmail.com>
2024-07-06 17:39:47 +0300

    created 3 cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import Card from "../components/cards"
 
 export default function Index() {
   return (
-    <main className="w-screen h-screen bg-zinc-900">
-      <div className="flex flex-row items-center justify-center h-full">
+    <main className="w-screen h-screen bg-zinc-900 overflow-hidden">
+      <div className="flex flex-row items-center justify-center h-full gap-4 m-4">
         <Card title="Welcome to Wisercash" description="Wisercash is a financial services company that provides financial solutions to consumers." />
+        <Card title="Our Vision" description="Our vision is to be the leading provider of financial solutions." />
+        <Card title="Our Mission" description="Our mission is to provide financial solutions to consumers." />
       </div>
     </main>
   );
--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ title, description }: { title: string, description: string }) => {
     return (
-        <div className="bg-stone-600 rounded-lg shadow-lg p-6">
+        <div className="bg-neutral-800 text-amber-50 rounded-lg shadow-lg p-6">
             <h2 className="text-xl font-bold">{title}</h2>
             <p>{description}</p>
         </div>
