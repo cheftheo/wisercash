d86bc6b52c2adef65a3fe8aea049ac21df24c11a
cheftheo <marascu.theodor@gmail.com>
2024-07-10 20:32:24 +0300

    styled comp + login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -11,23 +11,23 @@ const LoginPage = () => {
             <p className="text-amber-100">Please sign in to your account.</p>
 
             <form className="flex flex-col gap-4 mt-4">
-                <label className="text-amber-50" htmlFor="email"> Username </label>
-                <input className="focus:outline-0 rounded-lg p-2 bg-neutral-800 text-amber-100" type="email" id="email" name="email" />
+              <label className="text-amber-50" htmlFor="email"> Username </label>
+              <input className="focus:outline-0 rounded-lg p-2 bg-neutral-800 text-amber-100" type="email" id="email" name="email" />
 
-                <label className="text-amber-50" htmlFor="password">Password</label>
-                <input className="focus:outline-0 rounded-lg p-2 bg-neutral-800 text-amber-100" type="password" id="password" name="password"  />
+              <label className="text-amber-50" htmlFor="password">Password</label>
+              <input className="focus:outline-0 rounded-lg p-2 bg-neutral-800 text-amber-100" type="password" id="password" name="password"  />
 
-                <button className="transition-all bg-amber-50 text-neutral-900 rounded-lg p-2 hover:bg-amber-100" type="submit">
-                    Sign in
-                </button>
+              <button className="transition-all bg-amber-50 text-neutral-900 rounded-lg p-2 hover:bg-amber-100" type="submit">
+                  Sign in
+              </button>
 
-                <a className="transition-all text-amber-50 hover:text-amber-100" href="/auth/forgot-password">Forgot password?</a>
+              <a className="transition-all text-amber-50 hover:text-amber-100" href="/auth/forgot-password">Forgot password?</a>
             </form>
           </div>
         </div>
 
         <div className="w-1/2">
-            <AuthSec />
+          <AuthSec />
         </div>
       </div>
     </main>
--- src/components/authSec.tsx
+++ src/components/authSec.tsx
@@ -1,21 +1,60 @@
 "use client";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBullseye } from "@fortawesome/free-solid-svg-icons";
+import { faBullseye, faRocket, faFeather, faChild, faNewspaper } from "@fortawesome/free-solid-svg-icons";
 
 import { AnimatePresence, motion } from "framer-motion";
+// import Image from 'next/image'
+// import logo from '../../public/logo.png'
 
 export const AuthSec = () => {
   return (
     <AnimatePresence>
       <section>
         <motion.div>
-          <div className="p-16 w-4/6 h-full mt-24">
-            <div className="flex flex-col">
-              <div className="text-white inline-flex gap-2">
-                <FontAwesomeIcon icon={faBullseye} className="text-amber-200 text-4xl" />
-                <h1 className="text-3xl font-bold text-amber-50">Why us?</h1>
+          <div className="p-16 w-full h-full mt-16">
+            <div className="flex flex-col gap-y-12">
+
+              <div className="text-white">
+                <div className="inline-flex items-center gap-2">
+                  <FontAwesomeIcon icon={faBullseye} className="text-amber-200 text-4xl" />
+                  <h1 className="text-3xl font-bold text-amber-50">Why us?</h1>
+                  <p className="text-amber-100">We are the best in the business</p>
+                </div>
               </div>
+
+              <div className="text-white">
+                <div className="inline-flex items-center gap-2">
+                  <FontAwesomeIcon icon={faRocket} className="text-amber-200 text-4xl" />
+                  <h1 className="text-3xl font-bold text-amber-50">Why us?</h1>
+                  <p className="text-amber-100">Fatest online banking you ever had</p>
+                </div>
+              </div>
+              
+              <div className="text-white">
+                <div className="inline-flex items-center gap-2">
+                  <FontAwesomeIcon icon={faNewspaper} className="text-amber-200 text-4xl" />
+                  <h1 className="text-3xl font-bold text-amber-50">Why us?</h1>
+                  <p className="text-amber-100">Promotions and updates every day</p>
+                </div>
+              </div>
+
+              <div className="text-white">
+                <div className="inline-flex items-center gap-2">
+                  <FontAwesomeIcon icon={faChild} className="text-amber-200 text-4xl" />
+                  <h1 className="text-3xl font-bold text-amber-50">Why us?</h1>
+                  <p className="text-amber-100">A lightweight app for all user ages</p>
+                </div>
+              </div>
+              
+              <div className="text-white">
+                <div className="inline-flex items-center gap-2">
+                  <FontAwesomeIcon icon={faFeather} className="text-amber-200 text-4xl" />
+                  <h1 className="text-3xl font-bold text-amber-50">Why us?</h1>
+                  <p className="text-amber-100">Best support you ever had</p>
+                </div>
+              </div>
+
             </div>
           </div>
         </motion.div>
