272ba8240248b88aacd006c0e601d94ec1a02d8b
cheftheo <marascu.theodor@gmail.com>
2024-07-21 00:29:20 +0300

    added balance slider

--- src/app/dashboard/components/limit.tsx
+++ src/app/dashboard/components/limit.tsx
@@ -1,11 +1,12 @@
 export const LimitCard = (props: any) => {
-    const income = props.income;
-    const spend = props.spend;
+    const income = props.income || 0;
+    const spend = props.spend || 65;
 
     return ( 
         <div className="bg-zinc-800 text-white p-4 rounded-xl w-[100%] h-[100%] outline outline-1 outline-white/20">
-            <div className="flex items-center align-center justify-center">
-                <input type='range' max="100" value='55' className=""></input>
+            <div className="flex flex-col gap-3 align-center justify-self-center justify-center">
+                <h1 className="font-bold text-amber-200">Spend/Income balance</h1>
+                <input id="limitSlider" disabled={true} type="range" min="0" max="100" value={spend}></input>
             </div>
         </div>
     )
